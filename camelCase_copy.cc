68d4f6ba0944a72a18f9587af1e81465073a337f
Andrew Tang <andrewhltang@gmail.com>
2020-05-18 17:23:29 -0700

    Our first commit

---
+++ camelCase_copy.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using std::string;
+
+void convertString(int stringID, char* argv) {
+  string stringToConvert = string(argv);
+
+  size_t counter = 0; 
+  for (size_t i = 0; i < stringToConvert.size(); i++) {
+    if (isalpha(stringToConvert[i])) {
+      // int probability = rand() % 10; 
+      // std::cout << probability << std::endl;
+      // if (probability < 8) {
+        if (counter % 2 == 0) {
+          stringToConvert[i] = tolower(stringToConvert[i]); 
+        } else {
+          stringToConvert[i] = toupper(stringToConvert[i]);
+        }
+      // }
+      counter++;
+    }
+  }
+  // Copy to clipboard instead
+  std::cout << stringToConvert << std::endl;
+}
+		  
+
+int main(int argc, char* argv[]) {
+  // argv[i] is one string and is whatever is highlighted
+  for (size_t i = 1; i < argc; i++) {
+    convertString(i, argv[i]); 
+  }
+}
